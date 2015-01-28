961afe7dee83356d34b27098042ad880af12b79d
Marco Barbosa <iam@marcobarbosa.com>
2015-01-28 09:30:18 +0100

    about: putting the grid files in place

---
+++ app/_src/models/grid-model.js
@@ -0,0 +1,12 @@
+
+FOURTEEN.ABOUT_GRID_PATTERN_BLOCKS = {
+  'm': [[0, 1, 1, 0, 0], [0, 1, 1, 1, 1], [1, 0, 1, 1, 0]]
+};
+
+FOURTEEN.ABOUT_GRID_CELL_SIZES_BLOCKS = {
+  'm': {
+    0: 180,
+    1: 180,
+    2: 340
+  }
+};
