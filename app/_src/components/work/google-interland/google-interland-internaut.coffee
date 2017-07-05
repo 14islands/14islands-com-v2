970e1b95764b09a1da0de71844eb0f835ed01b01
David Lindkvist <david@14islands.com>
2017-07-05 14:15:47 +0200

    Added internaut component that plays animation only when in viewport

---
+++ app/_src/components/work/google-interland/google-interland-internaut.coffee
@@ -0,0 +1,68 @@
+###
+#
+# GoogleInterlandInternaut component.
+#
+# For Google Interland Case Study
+#
+# @author 14islands
+#
+###
+
+class FOURTEEN.GoogleInterlandInternaut extends FOURTEEN.ElementScrollVisibility
+
+	ROTATE_DISTANCE = 0.1 #degrees per frame
+
+	# define async scripts to load
+	scripts: [
+		'/js/google-interland-internaut/interland-internaut.min.js'
+	]
+
+
+	constructor: (@$context, data) ->
+		super(@$context, data)
+
+		@panoramaHasLoaded_ = false
+		@isScrolling_ = false
+
+		@spinner = new FOURTEEN.Spinner @$context, {isBlack: true}
+		@spinner.show()
+
+
+	# @protected
+	onReady: ->
+		super()
+
+		# we need a global callback for the maps api load event
+		FOURTEEN.onInterlandInternautLoaded = @_onInternautLoaded
+
+		# internaut already loaded? init directly
+		if FOURTEEN.InterlandInternaut
+			@_onInternautLoaded()
+
+
+	# @private
+	_onInternautLoaded: =>
+		@spinner.hide(=>
+			FOURTEEN.InterlandInternaut.init(@context)
+			@onEnterViewport() if @isInViewport
+		)
+
+	# @protected
+	destroy: ->
+		super()
+		FOURTEEN.InterlandInternaut.destroy()
+
+	# @protected
+	onEnterViewport: =>
+		super()
+		FOURTEEN.InterlandInternaut?.startRender()
+
+	# @protected
+	onFullyEnterViewport: =>
+		super()
+		FOURTEEN.InterlandInternaut?.triggerAnimation()
+
+	# @protected
+	onExitViewport: =>
+		super()
+		FOURTEEN.InterlandInternaut?.stopRender()
--- app/js/google-interland-internaut/src/main.js
+++ app/js/google-interland-internaut/src/main.js
@@ -18,12 +18,16 @@ const loadAsset = (name, file) => {
     });
 }
 
+let width
+let height
 let internaut
 let scene
 let renderer
 let camera
 let lookAt = new THREE.Object3D()
-lookAt.position.z = 25
+let contextEl
+// const clock = new THREE.Clock()
+let isRendering = false
 
 const animations = [
 	ANIMATIONS.INTERNAUT_WIGGLE,
@@ -37,12 +41,15 @@ function getRandomAnimation () {
   return animations[randomIndex]
 }
 
-const init = () => {
+
+const init = (el) => {
+  contextEl = el
+
   // create scene
   scene = new THREE.Scene()
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 1000 );
 	renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize( width, height );
   renderer.gammaInput = false
   renderer.gammaOutput = false
   renderer.physicallyBasedShading = false
@@ -51,7 +58,7 @@ const init = () => {
   renderer.setPixelRatio(window.devicePixelRatio)
 
   // add rendered canvas
-  document.querySelector('.js-interland').appendChild( renderer.domElement );
+  contextEl.appendChild(renderer.domElement)
 
   camera.position.z = 25;
   camera.position.y = 17;
@@ -63,35 +70,58 @@ const init = () => {
   internaut.mesh.scale.setScalar(0.3)
 
   // button
-  document.querySelector('.js-interland').addEventListener('click', (e) => {
-    const anim = getRandomAnimation()
-    console.log('trigger', anim)
-  	internaut.triggerAnimation(anim)
-	})
+  contextEl.addEventListener('click', triggerAnimation)
+
+  // resize
+  resize()
+  window.addEventListener('resize', resize)
 
   initLights()
   initLookAt()
+}
+
+function resize () {
+  width = contextEl.clientWidth
+  height = contextEl.clientHeight
+  renderer.setSize(width, height)
+  camera.aspect = width / height
+  camera.updateProjectionMatrix()
+}
+
+function startRender () {
+  isRendering = true
   render()
 }
-
-const clock = new THREE.Clock()
-function render() {
-  window.requestAnimationFrame(render)
-  const delta = 1000/60/1000 //clock.getDelta()
-	internaut.update(delta)
-  renderer.render( scene, camera );
+function stopRender () {
+  isRendering = false
+}
+function render () {
+  if (isRendering) {
+    window.requestAnimationFrame(render)
+  }
+  const delta = 1000 / 60 / 1000 // clock.getDelta()
+  internaut.update(delta)
+  renderer.render(scene, camera)
 }
 
-function initLookAt() {
-	internaut.updateLookAt(lookAt)
-  window.addEventListener('mousemove', (e) => {
-    const x = mathutil.normalize(e.clientX, 0, window.innerWidth)
-    const y = mathutil.normalize(e.clientY, 0, window.innerHeight)
-    const lookX = mathutil.interpolate(x, -10, 10)
-    const lookY = mathutil.interpolate(y, -50, 20)
-    lookAt.position.x = lookX
-    lookAt.position.y = lookY
-  })
+function triggerAnimation () {
+  const anim = getRandomAnimation()
+  internaut.triggerAnimation(anim)
+}
+
+function initLookAt () {
+  lookAt.position.z = 25
+  internaut.updateLookAt(lookAt)
+  window.addEventListener('mousemove', onMouseMoveLookAt)
+}
+
+function onMouseMoveLookAt (e) {
+  const x = mathutil.normalize(e.clientX, 0, width)
+  const y = mathutil.normalize(e.clientY, 0, height)
+  const lookX = mathutil.interpolate(x, -10, 10)
+  const lookY = mathutil.interpolate(y, -50, 20)
+  lookAt.position.x = lookX
+  lookAt.position.y = lookY
 }
 
 function initLights () {
@@ -127,6 +157,72 @@ function initLights () {
   scene.add(_ambientLight)
 }
 
+function disposeScene (scene) {
+  var rmArray = []
+
+  scene.traverse(function (object) {
+    rmArray.push(object)
+  })
+
+  for (let i = 0; i < rmArray.length; i++) {
+    let o = rmArray[i]
+    disposeObjectFromScene(o, scene)
+  }
+
+  rmArray.length = 0
+}
+
+function disposeObjectFromScene (o, scene) {
+  if (!o) {
+    return
+  }
+  try {
+    o.world = null
+
+    if (o.parent) {
+      o.parent.remove(o)
+    } else {
+      scene.remove(o)
+    }
+
+    if (o.geometry) {
+      o.geometry.dispose()
+    }
+
+    if (o.material instanceof THREE.MultiMaterial) {
+      for (let j = 0; j < o.material.materials.length; j++) {
+        o.material.materials[j].dispose()
+      }
+    } else if (o.material) {
+      o.material.dispose()
+    }
+  } catch (error) {
+    console.log('error disposing', error)
+  }
+}
+
+function destroy () {
+  stopRender()
+  renderer.dispose()
+  disposeScene(scene)
+  contextEl.removeEventListener('click', triggerAnimation)
+  window.removeEventListener('mousemove', onMouseMoveLookAt)
+}
+
+function onLoaded () {
+  // expose init function for 14islands.com
+  FOURTEEN.InterlandInternaut = {
+    init,
+    destroy,
+    startRender,
+    stopRender,
+    triggerAnimation
+  }
+
+  // notify 14islands.com that we loaded
+  FOURTEEN.onInterlandInternautLoaded && FOURTEEN.onInterlandInternautLoaded()
+}
+
 // load assets before init
 loadAsset('internaut', '/js/google-interland-internaut/data/internaut.json')
   .then(() => loadAsset('internaut_anim_idle', '/js/google-interland-internaut/data/internaut_anim_idle.json'))
@@ -136,4 +232,4 @@ loadAsset('internaut', '/js/google-interland-internaut/data/internaut.json')
 	.then(() => loadAsset('internaut_anim_superhappytrigger', '/js/google-interland-internaut/data/internaut_anim_superhappytrigger.json'))
 	.then(() => loadAsset('internaut_anim_wrongreported', '/js/google-interland-internaut/data/internaut_anim_wrongreported.json'))
 	.then(() => loadAsset('internaut_anim_bouncewall', '/js/google-interland-internaut/data/internaut_anim_bouncewall.json'))
-  .then(init)
+  .then(onLoaded)
